c5df35b09182161c4cf1b6de1af0c9de43c9d1f1
leoli <836864734@qq.com>
2020-10-14 18:15:48 +0800

    疯了！别再写满屏的 try catch 了！！

---
+++ kill-try-catch/src/main/java/leoli/killtrycatch/Application.java
@@ -0,0 +1,25 @@
+package leoli.killtrycatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author leoli
+ * @date 2020/10/13
+ */
+@RestController
+@SpringBootApplication
+public class Application {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
