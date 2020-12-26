bb4fde8fe6a87be2d7d33a5201923cc881bfc766
Leoli <836864734@qq.com>
2020-12-26 21:54:01 +0800

    springmvc encode

--- spring-annotation/spring-annotation/src/main/java/com/atguigu/bean/Dog.java
+++ spring-annotation/spring-annotation/src/main/java/com/atguigu/bean/Dog.java
@@ -4,7 +4,6 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
