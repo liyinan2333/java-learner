e909b7f444778d48643fcf18b79a99e701dbf220
LiYinan <liyinan2333@qq.com>
2022-02-10 02:36:29 +0800

    spring-data-jpa

--- spring-data-jpa/src/main/java/liyinan/jpa/repository/EmployeeQueryAnnotationRepository.java
+++ spring-data-jpa/src/main/java/liyinan/jpa/repository/EmployeeQueryAnnotationRepository.java
@@ -4,8 +4,10 @@ package liyinan.jpa.repository;
 import liyinan.jpa.bean.Employee;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * 注解或继承二选一
@@ -16,7 +18,21 @@ import java.util.Collection;
 //@RepositoryDefinition(domainClass = Employee.class, idClass = Integer.class)
 public interface EmployeeQueryAnnotationRepository extends Repository<Employee, Integer> {
 
-    @Query("select o from Employee o where id=(select max(id) from Employee t1)")
+    @Query("select o from Employee o where o.id=(select max(t1.id) from Employee t1)")
     Employee findEmployeeByMaxId();
 
+    @Query("select o from Employee o where o.name=?1 and o.age=?2")
+    List<Employee> findParams1(String name, Integer age);
+
+    @Query("select o from Employee o where o.name=:name and o.age=:age")
+    List<Employee> findParams2(@Param("name") String name, @Param("age") Integer age);
+
+    @Query("select o from Employee o where o.name like %?1%")
+    List<Employee> findLike1(String name);
+
+    @Query("select o from Employee o where o.name like %:name%")
+    List<Employee> findLike2(@Param("name") String name);
+
+    @Query(nativeQuery = true, value = "select count(1) from employee")
+    Integer getCount();
 }
--- spring-data-jpa/src/test/java/liyinan/jpa/repository/EmployeeQueryAnnotationRepositoryTest.java
+++ spring-data-jpa/src/test/java/liyinan/jpa/repository/EmployeeQueryAnnotationRepositoryTest.java
@@ -3,8 +3,13 @@ package liyinan.jpa.repository;
 import liyinan.jpa.bean.Employee;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 @DisplayName("JPA接口Query注解测试")
@@ -17,6 +22,40 @@ class EmployeeQueryAnnotationRepositoryTest {
     void findEmployeeByMaxId() {
         Employee employee = repository.findEmployeeByMaxId();
         System.out.println(employee);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"test1, 22"})
+    void queyParams1(String name, Integer age) {
+        List<Employee> employees = repository.findParams1(name, age);
+        employees.forEach(System.out::println);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"test1, 22"})
+    void queyParams2(String name, Integer age) {
+        List<Employee> employees = repository.findParams2(name, age);
+        employees.forEach(System.out::println);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"test1"})
+    void findLike1(String name) {
+        List<Employee> employees = repository.findLike1(name);
+        employees.forEach(System.out::println);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"test1"})
+    void findLike2(String name) {
+        List<Employee> employees = repository.findLike2(name);
+        employees.forEach(System.out::println);
+    }
+
+    @Test
+    void getCount() {
+        Integer count = repository.getCount();
+        System.out.println(count);
     }
 
 }
