6a426a9c00a3bd1076759d56615eb06e3eb66c23
LiYinan <liyinan2333@qq.com>
2022-11-28 18:06:09 +0800

    activemq-spring-boot

---
+++ MQ/ActiveMQ/activemq-springboot/src/main/java/com/liyinan/rabbitmq/producer/Producer.java
@@ -0,0 +1,46 @@
+package com.liyinan.rabbitmq.producer;
+
+import com.liyinan.rabbitmq.config.QueueConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.jms.Queue;
+import javax.jms.Topic;
+
+@Slf4j
+@RestController
+@RequestMapping("send")
+public class Producer {
+
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    @Autowired
+    @Qualifier(QueueConfig.QUEUE_ID)
+    Queue queue;
+
+    @Autowired
+    @Qualifier(QueueConfig.TOPIC_ID)
+    Topic topic;
+
+    @RequestMapping("queue")
+    public ResponseEntity sendToQueue(@RequestParam("message") String message) {
+        log.info("<o< {} context->{}", queue, message);
+        jmsTemplate.convertAndSend(queue, message);
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping("topic")
+    public ResponseEntity sendToTopic(@RequestParam("message") String message) {
+        log.info("<o< {} context->{}", topic, message);
+        jmsTemplate.convertAndSend(topic, message);
+        return ResponseEntity.ok().build();
+    }
+
+}
